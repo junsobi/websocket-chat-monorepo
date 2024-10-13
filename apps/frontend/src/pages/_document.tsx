ba939288c2fb51475533197f66b80c45acfa17d2
junsobi <junsobi@gmail.com>
2024-10-13 23:07:44 +0900

    Refactor language setting to Korean in _document.tsx

--- apps/frontend/src/pages/_document.tsx
+++ apps/frontend/src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="kor">
       <Head />
       <body className="antialiased">
         <Main />
